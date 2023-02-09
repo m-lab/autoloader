21998c43911aa4fde4309e0ee251d68ce9be76fc
Cristina Leon <cristinaleon@google.com>
2023-02-09 14:57:08 +0000

    Add autoloader bq client

---
+++ api/datatype.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"time"
+
+	"github.com/m-lab/go/storagex"
+)
+
+// Datatype defines an individual datatype whose existence is denoted
+// by the existence of a schema file under the path
+// `autoload/v1/tables/<Experiment>/<Datatype>.table.json` within a GCS bucket.
+//
+// For each Datatype, there will be a BigQuery table named `Project:Experiment.Name`
+// with the contents under `autoload/v1/<Experiment>/<Datatype>/*`.
+type Datatype struct {
+	Name        string           // Datatype name (e.g., "ndt7")
+	Experiment  string           // Experiment name (e.g., "ndt")
+	Location    string           // Bucket location (e.g., "us-east")
+	Schema      []byte           // Contents of schema file in GCS.
+	UpdatedTime time.Time        // Last time the schema was updated in GCS.
+	Bucket      *storagex.Bucket // GCS Bucket.
+}
